3e8a5d311a7acc5ca430b6314006bda4e5eeb6d9
sgujuluva <sanghee04@gmail.com>
2022-08-13 20:36:50 +0200

    card component created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 //lib react
-import {useState} from "react";
+import { useState, useEffect } from "react";
 //styles
 import "./App.css";
 
-//fetching api
+/* //fetching api
 const apiFetch = async () => {
   let response = await fetch("https://www.googleapis.com/books/v1/volumes?q='+search+&key=AIzaSyBGxpDYEOpN8ihF0iRPm2BqRa5EBfJq-yk");
   let data= await response.json();
   return data;
-}
+} */
 function App() {
   //state for user input search
-const [search,setSearch] = useState("");
-//state for data from api
-const [bookData,setBookData] = useState([]);
+  const [search, setSearch] = useState("");
+  //state for data from api
+  const [bookData, setBookData] = useState([]);
 
-//search click function
-const handleSubmit = (e) => {
-  e.preventDefault();
-  console.log(search)
-}
+  const searchBook = (evt) => {
+    if (evt.key === "Enter") {
+      fetch(
+        `https://www.googleapis.com/books/v1/volumes?q=` +
+          search +
+          `&key=AIzaSyBGxpDYEOpN8ihF0iRPm2BqRa5EBfJq-yk`
+      ).then((res) =>  setBookData(res.data.items))
+      .catch(err => console.log("error"))
+    }
+  };
+  
+  /* //useeffect to fetch api
+useEffect(() => {
+apiFetch(search)
+.then(result => setBookData(result.data.items) )
+
+},[])
+console.log(bookData) */
+
+  //search click function
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
   return (
     <div className="App">
@@ -30,7 +48,13 @@ const handleSubmit = (e) => {
         <div className="search">
           <h2>Search a Book</h2>
           <form onSubmit={handleSubmit}>
-            <input onChange={(e) => setSearch(e.target.value)} type="text" value = {search} placeholder="Type a Book name" />
+            <input
+              onChange={(e) => setSearch(e.target.value)}
+              type="text"
+              value={search}
+              placeholder="Type a Book name"
+               onKeyPress={searchBook}
+            />
             <button>Search</button>
           </form>
         </div>
