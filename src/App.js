d742fef8160b74e09c2315ba5392800175e73a55
sgujuluva <sanghee04@gmail.com>
2022-08-13 18:15:48 +0200

    header and search element written in jsx

--- src/App.js
+++ src/App.js
@@ -4,7 +4,19 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+    <div className="header">
+      <div className="heading">
+      <h1>Books are a uniquely Portable magic</h1>
+      </div>
+  <div className="search">
+    <h2>Search a Book</h2>
+    <form>
+      <input type="text" placeholder="Type a Book name" />
+      <button>Search</button>
+    </form>
+  </div>
+    </div>
+     
     </div>
   );
 }
