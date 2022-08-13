381b69b17628cf6c2e91411001d3950f78a2eb59
sgujuluva <sanghee04@gmail.com>
2022-08-13 18:37:58 +0200

    styling header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 //styles
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    <div className="header">
-      <div className="heading">
-      <h1>Books are a uniquely Portable magic</h1>
+      <div className="header">
+        <div className="heading">
+          <h1>Books are a uniquely Portable magic</h1>
+        </div>
+        <div className="search">
+          <h2>Search a Book</h2>
+          <form>
+            <input type="text" placeholder="Type a Book name" />
+            <button>Search</button>
+          </form>
+        </div>
       </div>
-  <div className="search">
-    <h2>Search a Book</h2>
-    <form>
-      <input type="text" placeholder="Type a Book name" />
-      <button>Search</button>
-    </form>
-  </div>
-    </div>
-     
     </div>
   );
 }
